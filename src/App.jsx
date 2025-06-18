e2f98d4be4bd615cfdaeace3292ffc41bd2221aa
Toutvendre <ducvfx07@gmail.com>
2025-06-18 12:50:21 +0000

    premier commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1>Bienvenu</h1>
+      <Button variant="default">Click moi</Button>
+    </div>
+  )
+}
+
+export default App
